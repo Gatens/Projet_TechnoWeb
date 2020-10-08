584028c3953838736259d945c1342677355d04ba
Gatien <gatien.soetens@isen.yncrea.fr>
2020-10-08 15:44:43 +0200

    lien css plus go back homepage

--- account.php
+++ account.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
+<link rel="stylesheet" type="text/css" href="main.css" />
   <head>
     <meta charset="utf-8">
     <title>Account</title>
@@ -27,7 +28,7 @@
 
         <footer>
             <div class="footer">
-              <a href="homepage.html" class="botText">goback to homepage</a>
+              <a href="homepage.php" class="botText">goback to homepage</a>
             </div>
         </footer>
 
