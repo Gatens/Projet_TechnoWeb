d883607a814d6ad4aff0f20b1b6e64723c5d235f
Gatien <gatien.soetens@isen.yncrea.fr>
2020-10-08 14:45:01 +0200

    j'avais oublie ca

--- homepage.php
+++ homepage.php
@@ -10,14 +10,14 @@
     <p> Bonjour, bienvenue sur notre site !
         Vous pouvez vous entrainez sur nos quizz sous format QCM afin de vous perfectionner comme nous nous perfectionnons nous aussi en html css et php.</p>
         <img src="quizz.png"/img><br>
-    <a href="compte.html" class="onglet">Account</a><br>
-    <a href="quizz1.html"  class="onglet">Quizz1</a><br>
-    <a href="quizz2.html"  class="onglet">Quizz2</a><br>
+    <a href="compte.php" class="onglet">Account</a><br>
+    <a href="quizz1.php"  class="onglet">Quizz1</a><br>
+    <a href="quizz2.php"  class="onglet">Quizz2</a><br>
 
 
     <footer>
         <div class="/.footer">
-          <a href="homepage.html" class="botText">goback to homepage</a>
+          <a href="homepage.php" class="botText">goback to homepage</a>
         </div>
     </footer>
   </body>
