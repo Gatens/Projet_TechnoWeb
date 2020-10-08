f720344bbb50091bf78da938c58088b212a5f653
Grosbety <benjamin.grosbety@isen.yncrea.fr>
2020-10-08 18:06:12 +0200

    bouton quizz

--- homepage.php
+++ homepage.php
@@ -1,19 +1,42 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <link rel="stylesheet" type="text/css" href="main.css" /> 
+
   <head>
     <meta charset="utf-8">
     <title>HomePage</title>
   </head>
+
   <body>
+
     <div class="container text">
     <a href="#" class="title">Homepage</a>
     <p> Bonjour, bienvenue sur notre site !
         Vous pouvez vous entrainez sur nos quizz sous format QCM afin de vous perfectionner comme nous nous perfectionnons nous aussi en html css et php.</p>
+
         <img src="quizz.png"/img><br>
+
+        <div class="bouton">
+          <a href="quizz1.php" class="bouton1">QUIZZ 1</a><br>
+          <a href="quizz2.php" class="bouton2">QUIZZ 2</a><br>
+        </div>
+          
+      
+          
+  
+
     <a href="account.php" class="onglet">Account</a><br>
-    <a href="quizz1.php"  class="onglet">Quizz1</a><br>
-    <a href="quizz2.php"  class="onglet">Quizz2</a><br>
+
+    
+
+
+
+  
+
+    
+
+         
+    
 
 
     <footer>
@@ -21,5 +44,8 @@
           <a href="homepage.php" class="botText">goback to homepage</a>
         </div>
     </footer>
+
+
   </body>
+
 </html>
