62f9ccac5ed78cde23a0c26dfb10b6f14f6508ad
Gatien <gatien.soetens@isen.yncrea.fr>
2020-10-08 14:42:35 +0200

    voila le php

--- account.php
+++ account.php
@@ -1,3 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Account</title>
+  </head>
+
+  <body>
+
+  	<div id="account">
+
+            <h1>
+                Connectez-vous
+            </h1>
+
+            <form method="post">
+                <p>
+                    <label for="nom">Name</label> : <input type="text" name="name" placeholder="Ex: La B" id="name" required />
+                    <label for="surname">Surname</label> : <input type="text" name="surname" placeholder="Ex: Thomas" id="surname" required />
+                    <input type="submit" name="OK">
+                </p>
+            </form>
+
+        </div>
+
+
+
+        <footer>
+            <div class="/.footer">
+              <a href="homepage.html" class="botText">goback to homepage</a>
+            </div>
+        </footer>
+
+
+  </body>
+
+
+</html>
+
+
 <?php
 $bonjour='bonjour ';
 $name=$_POST['name'];
---
+++ quizz1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <footer>
+        <div class="/.footer">
+          <a href="homepage.html" class="botText">goback to homepage</a>
+        </div>
+    </footer>
+  </body>
+</html>
