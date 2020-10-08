da4d461ba3f07d2ef2b310e7ae40b7d5e5b8f7be
Grosbety <benjamin.grosbety@isen.yncrea.fr>
2020-10-08 15:45:02 +0200

    link page quiz

--- quizz1.php
+++ quizz1.php
@@ -7,7 +7,7 @@
   <body>
     <footer>
         <div class="footer">
-          <a href="homepage.html" class="botText">goback to homepage</a>
+          <a href="homepage.php" class="botText">goback to homepage</a>
         </div>
     </footer>
   </body>
