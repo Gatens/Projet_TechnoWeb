d9822038525187c616435325846b54f13fdba37d
Grosbety <benjamin.grosbety@isen.yncrea.fr>
2020-10-23 16:47:47 +0200

    remplir bdd via php

--- remplissagebdd.php
+++ remplissagebdd.php
@@ -1,12 +1,25 @@
 <html>
 
+    <?php
+
+        $user = 'root';
+
+
+        try{
+            $db = new PDO ('mysql:host=localhost;dbname=php', $user);
+            print "Connecté";
+        }
+        catch (PDOException $e0){
+            print "Erreur :". $e->getMessage() . "<br/>";
+            die;
+        } 
+    ?>
+
     <body>
 
         <?php
         // lancement de la requete
         $sql = 'INSERT INTO question VALUES (",Moscou,1,")';
-
-        mysql_query ($sql) or die ('Erreur SQL !'.$sql.'<br />'.mysql_error());
 
         // on ferme la connexion à la base
         mysql_close();
