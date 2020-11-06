e0839d874bcf8ac2f5083a73bc9631950b2ffec8
Gatien <gatien.soetens@isen.yncrea.fr>
2020-11-06 18:47:28 +0100

    login ok

--- pages/login.php
+++ pages/login.php
@@ -7,8 +7,8 @@ if (isset($_POST["Envoyer"])){
   //echo $email;
   //echo $pass;
   // verif reussi
-  $user = $bdd->query('SELECT email,password FROM user;')->fetchAll(); // recuperation du mail et du password dans la bdd
-
+  $user = $bdd->query('SELECT usermail,password FROM user;')->fetchAll(); // recuperation du mail et du password dans la bdd
+  //var_dump ($user);
 
   /*var_dump($user);
   foreach ($user as $value) {
@@ -20,9 +20,9 @@ if (isset($_POST["Envoyer"])){
 */
   function checkUser($db, $userMail, $userPass){
     foreach ($db as $value){ // on parcours toute l'array
-      if ($value['email']==$userMail){ // on verifie que l'email appartient a la database
+      if ($value['usermail']==$userMail){ // on verifie que l'email appartient a la database
         //echo $value['password'];
-        if ($value['password']==$userPass){ // on verifie que le password corresponf a cet adresse mail
+        if (password_verify($userPass,$value['password'])){ // on verifie que le password corresponf a cet adresse mail
           echo "connexion effectue <br/>";
         }
         else {
