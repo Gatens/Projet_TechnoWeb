6626e83c5c2db016c1b73c56323792e489b23032
Gatens <gatien.soetens@gmail.com>
2020-11-07 19:39:32 +0100

    image homepage cliquable

--- pages/homepage.php
+++ pages/homepage.php
@@ -10,9 +10,9 @@
   <body>
 
     <div id="imageQuizz">
-      <img class="imageQuizz1" src="img/quizz1.png">
+      <a href="index.php?page=quizz&id=1"><img class="imageQuizz1" src="img/quizz1.png"></a>
       <img class="imageQuizz" src="img/quizz.png">
-      <img class="imageQuizz2" src="img/quizz2.png">
+      <a href="index.php?page=quizz&id=2"><img class="imageQuizz2" src="img/quizz2.png"></a>
     </div>
 
 </html>
