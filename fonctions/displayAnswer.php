ca82d59371318596b3397e5be3486b439e13f730
champion <clem.champion59@gmail.com>
2020-11-08 20:35:12 +0100

    petits changement

--- fonctions/displayAnswer.php
+++ fonctions/displayAnswer.php
@@ -1,59 +1,76 @@
 <?php
-		function compare($question_id,$answerArray,$bdd){    // fonction qui compare les réponses avec la bdd
-  		$response = $bdd->query('SELECT answer_id,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$question_id)->fetchAll();
-  		$question = $bdd->query('SELECT question_input_type FROM question WHERE question_id ='.$question_id)->fetchAll();
-  		$compGoodAns=0;
-  		foreach ($response as $value1) {
-    		if ($value1['is_valid_answer']) {
-        		$compGoodAns=$compGoodAns+1; // ajoute au compteur de bonne réponse
-    		}
+		function compare($question_id,$answerArray,$bdd)// fonction qui compare les réponses avec la bdd
+		{
+  		$answer = $bdd->query('SELECT answer_id,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$question_id)->fetchAll();
+
+			$question = $bdd->query('SELECT question_input_type FROM question WHERE question_id ='.$question_id)->fetchAll();
+
+  		$good_answers=0;
+
+  		foreach ($answer as $test)
+			{
+    		if ($test['is_valid_answer'])
+				 {
+        		$good_answers=$good_answers+1; // ajoute au compteur de bonne réponse
+    		 }
   		}
 
-  		$comp=0;
-  		foreach($response as $value){   // ajoute au compteur de réponses
-    		if( in_array($value['answer_id'],$answerArray)&&($value['is_valid_answer'])){
-      		$comp=$comp+1;
+  		$counter=0;
+  		foreach($answer as $test2) // ajoute au compteur de réponses
+			{
+    		if(in_array($test2['answer_id'],$answerArray)&&($test2['is_valid_answer']))
+				{
+      		$counter=$counter+1;
     		}
   		}
-  		if($comp==$compGoodAns){   // affiche bonne ou mauvaise réponse en fonction de l'égalité des compteurs
-    		return ' Réponse Correcte';
-  		}else{
-    		return ' Mauvaise Réponse';
+  		if($counter==$good_answers) // affiche bonne ou mauvaise réponse en fonction de l'égalité des compteurs
+			{
+    		return ' Réponse Correcte'; //bug pour sélection et input
+  		}
+			else
+			{
+    		return ' Réponse incorrecte';
   		}
 		}
 
-	function displayAnswer($quizzId,$bdd){ // affiche les réponses en html en fonction de leur type et en les comparant avec la database
+
+
+	function displayAnswer($id_quizz,$bdd)// affiche les réponses en html en fonction de leur type et en les comparant avec la database
+	{
 
             $quizz = $bdd->query('SELECT quizz_name FROM quizz;')->fetchAll(); // affiche le nom du quizz
-            echo('<div id="content"><div id="titrePage"><h2>Quizz '.$quizz[$quizzId-1]['quizz_name'].'</h2></div>');
+            echo('<div id="content"><div id="titrePage"><h2>Quizz '.$quizz[$id_quizz-1]['quizz_name'].'</h2></div>');
 
 						// affiche les questions
 
-            $question = $bdd->query('SELECT question_id, question_title,question_input_type,question_quizz_id FROM question WHERE question.question_quizz_id = '.$quizzId)->fetchAll();
-            $comp=0; // numéro de la question
-
-            foreach ($question as $key=>$line){
-              $comp=$comp+1;
-
-							// Type selection
-
-              if($line['question_input_type']=='selection'){
-                echo("<div id='question1".$comp."_quizz1' class='question1'>");
-
-                echo("<p class='question1'>Question ".$comp." : ".$line['question_title']."".compare($line['question_id'],$_POST,$bdd)."</p>");
-
-                echo(" <select  name='Question".$comp."Quizz".$line['question_quizz_id']."' form='selection'>");
+            $question = $bdd->query('SELECT question_id, question_title,question_input_type,question_quizz_id FROM question WHERE question.question_quizz_id = '.$id_quizz)->fetchAll();
+						// on fait une requete pour la question dans la bdd
+					  $question_number=0; // numéro de la question
+							// on fait une requete pour la reponse dans la bdd
+  					$answer = $bdd->query('SELECT answer_id,answer_text,is_valid_answer, answer_question_id FROM answer;')->fetchAll();
 
 
+            foreach ($question as $key=>$type)
+						{
+              $question_number=$question_number+1;
 
+							/*_________________________________SELECTION_______________________________________*/
 
-                $response = $bdd->query('SELECT answer_id,answer_text,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$line['question_id'])->fetchAll();
+              if($type['question_input_type']=='selection')
+							{
+                echo("<div id='question1".$question_number."_quizz1' class='question1'>");
+								// affiche le numéro de la question
+                echo("<p class='question1'>Question ".$question_number." : ".$type['question_title']."".compare($type['question_id'],$_POST,$bdd)."</p>");
+								// affiche  la question
+                echo(" <select  name='Question".$question_number."Quizz".$type['question_quizz_id']."' form='selection'>");
+									// on fait une requete pour la reponse dans la bdd
+								$answer = $bdd->query('SELECT answer_id,answer_text,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$type['question_id'])->fetchAll();
 
-
-
-                foreach ($response as $key2 => $answer) {  // vérifie que la réponse est correcte
-                    if ($answer['is_valid_answer'] == 1){
-                      echo('<option value="true">'.$answer['answer_text'].'</option>'); // affiche le résultat
+                foreach ($answer as $key2 => $proposition) // vérifie que la réponse est correcte
+								 {
+                    if ($proposition['is_valid_answer'] == 1)
+										{
+                      echo('<option value="true">'.$proposition['answer_text'].'</option>'); // affiche le résultat
                     }
                 }
 
@@ -61,56 +78,69 @@
                 echo("</div>");
               }
 
-								// Type chechbox
+							/*_________________________________CHECKBOX_______________________________________*/
 
-              if($line['question_input_type']=='checkbox'){
-                echo("<div id='question1".$comp."_quizz1' class='question1'>");
-                echo("<p class='question1'>Question ".$comp." : ".$line['question_title']."".compare($line['question_id'],$_POST,$bdd)."</p>");
-                $response = $bdd->query('SELECT answer_id,answer_text,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$line['question_id'])->fetchAll();
-                $compans=0;
+              if($type['question_input_type']=='checkbox')
+							{
+                echo("<div id='question1".$question_number."_quizz1' class='question1'>");
+								echo("<p class='question1'>Question ".$question_number." : ".$type['question_title']."".compare($type['question_id'],$_POST,$bdd)."</p>");
 
-                foreach ($response as $key2 => $answer) {
-                    $compans=$compans+1;
-                    if ($answer['is_valid_answer'] == 1){
+                $answer = $bdd->query('SELECT answer_id,answer_text,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$type['question_id'])->fetchAll();
+									// on fait une requete pour la reponse dans la bdd
+								$answer_counter=0;
 
-                            echo("<div> <input type='checkbox' checked id='rep".$compans."1q1' name='rep".$compans."'> <label for='rep1q1'>".$answer['answer_text']."</label></div>");
+                foreach ($answer as $key2 => $proposition)
+								 {
+                    $answer_counter=$answer_counter+1;
+                    if ($proposition['is_valid_answer'] == 1)
+										{
+
+                            echo("<div> <input type='checkbox' checked id='rep".$answer_counter."' name='rep".$answer_counter."'> <label for='rep'>".$proposition['answer_text']."</label></div>");
 
                     }
                 }
 
                 echo('</div>');
-                $compans=0;
+                $answer_counter=0;
               }
 
-								// Type input
+							/*_________________________________INPUT_______________________________________*/
 
-              if($line['question_input_type']=='input'){
-                $response = $bdd->query('SELECT answer_id,answer_text,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$line['question_id'])->fetchAll();
+              if($type['question_input_type']=='input')
+							{
+                $answer = $bdd->query('SELECT answer_id,answer_text,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$type['question_id'])->fetchAll();
+									// on fait une requete pour la reponse dans la bdd
+                echo("<div id='question1".$question_number."_quizz1' class='question1'>");
+                echo("<p class='question1'>Question ".$question_number." : ".$type['question_title']."".compare($type['question_id'],$_POST,$bdd)."</p>");
 
-                echo("<div id='question1".$comp."_quizz1' class='question1'>");
-                echo("<p class='question1'>Question ".$comp." : ".$line['question_title']."".compare($line['question_id'],$_POST,$bdd)."</p>");
-                foreach ($response as $key2 => $answer) {
-                    echo('<input id="GET-name" value="'.$answer['answer_text'].'" type="text" name="name">');
+                foreach ($answer as $key2 => $proposition)
+								{
+                    echo('<input id="GET-name" value="'.$proposition['answer_text'].'" type="text" name="name">');
                 }
                 echo('</div>');
               }
 
-								// Type radio
+								/*_________________________________RADIO_______________________________________*/
 
-              if($line['question_input_type']=='radio'){
-                echo("<div id='question1".$comp."_quizz1' class='question1'>");
-                echo("<p class='question1'>Question ".$comp." : ".$line['question_title']."".compare($line['question_id'],$_POST,$bdd)."</p>");
-                $response = $bdd->query('SELECT answer_id,answer_text,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$line['question_id'])->fetchAll();
+              if($type['question_input_type']=='radio')
+							{
+                echo("<div id='question1".$question_number."_quizz1' class='question1'>");
+                echo("<p class='question1'>Question ".$question_number." : ".$type['question_title']."".compare($type['question_id'],$_POST,$bdd)."</p>");
 
-                foreach ($response as $key2 => $answer) {
-                    if ($answer['is_valid_answer'] == 1){
-                    echo('<input type="radio" checked name="radio" class="radio"> <label for="radio">'.$answer['answer_text'].'</label> <br/>');
+                $response = $bdd->query('SELECT answer_id,answer_text,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$type['question_id'])->fetchAll();
+									// on fait une requete pour la reponse dans la bdd
+                foreach ($response as $key2 => $proposition)
+								{
+                    if ($proposition['is_valid_answer'] == 1)
+										{
+                    echo('<input type="radio" checked name="radio" class="radio"> <label for="radio">'.$proposition['answer_text'].'</label> <br/>');
                     }
                 }
 
                 echo('</div>');
               }
             }
+						// on submit et on renvoie à la home page
             echo('<div class="boutonSubmit"><a href="index.php?page=home"> <input type="submit" value="Accueil"class="buttonSubmit"></a></div>');
             echo("</div>");
             echo("</div>");
--- fonctions/displayQuizz.php
+++ fonctions/displayQuizz.php
@@ -1,12 +1,14 @@
 <?php
 
-	function displayQuizz($ID_quizz,$bdd){  /*fonction qui affiche le quizz correspondant à son ID*/
+	function displayQuizz($ID_quizz,$bdd)
+	{  /*fonction qui affiche le quizz correspondant à son ID*/
 
-    function quizzName ($ID_quizz,$compteur,$value,$goodQuestion,$bdd){
+    	function quizzName ($ID_quizz,$question_number,$value,$goodQuestion,$bdd)
+			{
 
-      echo("<div id='content ".$compteur."_quizz".$ID_quizz."' class='question1 ".$goodQuestion."'>");
-      echo("<p class='question1'>Question ".$compteur." : ".$value['question_title']."</p>");
-    }
+      echo("<div id='content ".$question_number."_quizz".$ID_quizz."' class='question1 ".$goodQuestion."'>");
+      echo("<p class='question1'>Question ".$question_number." : ".$value['question_title']."</p>");
+    	}
 
 
     $quizz = $bdd->query('SELECT quizz_name FROM quizz;')->fetchAll();  // selectionne le nom du quizz
@@ -17,74 +19,78 @@
 		// selectionne les questions correspondant à l'ID du quizz
 
     $question = $bdd->query('SELECT question_id, question_title,question_input_type,question_quizz_id FROM question WHERE question.question_quizz_id = '.$ID_quizz)->fetchAll();
-    $compteur=0; //numéro de la question //
+    $question_number=0; //numéro de la question //
 
-    foreach ($question as $key=>$value){
-      $compteur=$compteur+1;
+    foreach ($question as $key=>$value)
+		{
+      $question_number=$question_number+1;
 
-      $questionExacte="q".$compteur."f".$ID_quizz."";
+      $questionExacte="q".$question_number."f".$ID_quizz."";
 
-      // Pour chaque Type
+			/*_________________________________SELECTION_______________________________________*/
 
-			//Type selection//
+      if($value['question_input_type']=='selection')
+			{
 
-      if($value['question_input_type']=='selection'){
+        quizzName ($ID_quizz,$question_number,$value,$questionExacte,$bdd);
 
+        echo(" <select  name='Question".$question_number."Quizz".$value['question_quizz_id']."' class='selection' form='selection'>");
 
-        quizzName ($ID_quizz,$compteur,$value,$questionExacte,$bdd);
+        $answer = $bdd->query('SELECT answer_id,answer_text,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$value['question_id'])->fetchAll();
 
-        echo(" <select  name='Question".$compteur."Quizz".$value['question_quizz_id']."' class='selection' form='selection'>");
-
-        $response = $bdd->query('SELECT answer_id,answer_text,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$value['question_id'])->fetchAll();
-
-        foreach ($response as $key2 => $answer) {
-          echo('<option value=' .$answer['answer_id']. '>'.$answer['answer_text'].'</option>');
+        foreach ($answer as $key2 => $proposition)
+				{
+          echo('<option value=' .$proposition['answer_id']. '>'.$proposition['answer_text'].'</option>');
         }
 
         echo("</select>");
         echo("</div>");
       }
 
-			//Type input//
-		  if($value['question_input_type']=='input'){
-
-		        quizzName ($ID_quizz,$compteur,$value,$questionExacte,$bdd);
+			/*_________________________________INPUT_______________________________________*/
+		  if($value['question_input_type']=='input')
+			{
+		        quizzName ($ID_quizz,$question_number,$value,$questionExacte,$bdd);
 
 		        echo('<input id="GET-name" class="input" type="number" name="name">');
 		        echo('</div>');
-		      }
-			//Type radio//
+		  }
+			/*_________________________________RADIO_______________________________________*/
 
 
-					if($value['question_input_type']=='radio'){
+					if($value['question_input_type']=='radio')
+					{
 
-						quizzName ($ID_quizz,$compteur,$value,$questionExacte,$bdd);
+						quizzName ($ID_quizz,$question_number,$value,$questionExacte,$bdd);
 
 
-						$response = $bdd->query('SELECT answer_id,answer_text,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$value['question_id'])->fetchAll();
+						$answer = $bdd->query('SELECT answer_id,answer_text,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$value['question_id'])->fetchAll();
 
-						foreach ($response as $key2 => $answer) {
-							echo('<input type="radio" class="radioElmnt" name="radio" value='.$answer['answer_id'].' class="radio"> <label for="radio">'.$answer['answer_text'].'</label> <br/>');
+						foreach ($answer as $key2 => $proposition)
+						 {
+							echo('<input type="radio" class="radioElmnt" name="radio" value='.$proposition['answer_id'].' class="radio"> <label for="radio">'.$proposition['answer_text'].'</label> <br/>');
 						}
 
 						echo('</div>');
 					}
-				//Type checkbox//
+				/*_________________________________CHECKBOX_______________________________________*/
 
-					if($value['question_input_type']=='checkbox'){
+					if($value['question_input_type']=='checkbox')
+					{
 
-						quizzName ($ID_quizz,$compteur,$value,$questionExacte,$bdd);
+						quizzName ($ID_quizz,$question_number,$value,$questionExacte,$bdd);
 
-						$response = $bdd->query('SELECT answer_id,answer_text,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$value['question_id'])->fetchAll();
-						$compteurans=0;
+						$answer = $bdd->query('SELECT answer_id,answer_text,is_valid_answer FROM answer WHERE answer.answer_question_id ='.$value['question_id'])->fetchAll();
+						$answer_counter=0;
 
-						foreach ($response as $key2 => $answer) {
-							$compteurans=$compteurans+1;
-							echo("<div> <input class='checkboxElmnt' type='checkbox' id='rep".$compteurans."1q1' name='choix[]'' value=".$answer['answer_id']." '> <label for='rep1q1'>".$answer['answer_text']."</label></div>");
+						foreach ($answer as $key2 => $proposition)
+						{
+							$answer_counter=$answer_counter+1;
+							echo("<div> <input class='checkboxElmnt' type='checkbox' id='rep".$answer_counter."1q1' name='choix[]'' value=".$proposition['answer_id']." '> <label for='rep1q1'>".$proposition['answer_text']."</label></div>");
 						}
 
 						echo('</div>');
-						$compteurans=0;
+						$answer_counter=0;
 					}
 
 
