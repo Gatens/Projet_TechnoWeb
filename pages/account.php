13a22e0a555919f034d59f2b5451bb4f5d0c67fc
Gatien <gatien.soetens@isen.yncrea.fr>
2020-11-22 15:41:46 +0100

    verif unicite compte utilisateur

--- pages/account.php
+++ pages/account.php
@@ -5,12 +5,12 @@
   <form method="post" action="index.php?page=login">
     <p>
       <label for="id">email</label> : <input type="text" name="email" placeholder="Entrer identifiant" id="id" required />
-      <label for="psw">password</label> : <input type="password" name="pass" placeholder="*******" id="psw" required />
+      <label for="psw">password</label> : <input type="password" name="pass" placeholder="***" id="psw" required />
 
       <button type="submit" name="Connexion">Connexion</button>
     </p>
   </form>
-  
+
   <form method="post" action="index.php?page=register">
     <h1> S'enregistrer ? </h1>
     <button type="submit" name="Register">Register</button>
--- pages/createUser.php
+++ pages/createUser.php
@@ -1,23 +1,57 @@
 <?php
 
-if (isset($_POST["Envoie"])){
-  $usermail =$_POST['email'];
-  $userpass=$_POST['password'];
-  $username=$_POST['username'];
-  $name=$_POST['name'];
-  $surname=$_POST['surname'];
-  header('Location: index.php?page=account'); // redirige vers la poge account
+$db = $bdd->query('SELECT usermail,username FROM user;')->fetchAll(); // recuperation du mail et du username dans la bdd
 
-  $userpass = password_hash($userpass,PASSWORD_DEFAULT);
+function checkExist($db,$username,$usermail){
+  $exist = 0;
+    foreach ($db as $value){ // on parcours toute l'array
+      if ($value['usermail']==$usermail){ // on verifie que l'email appartient a la database
+        $exist = 1;
+        $message='Cet email est deja enregistré sur notre site !';
+        echo '<script type="text/javascript">window.alert("'.$message.'");
+        window.location.href="index.php?page=register";
+        </script>';
+      }
 
-  $create=$bdd->prepare('INSERT INTO user VALUES (NULL, :usermail,:userpass,:username,:name,:surname)');
-  $create->bindParam(':usermail',$usermail);
-  $create->bindParam(':userpass',$userpass);
-  $create->bindParam(':username',$username);
-  $create->bindParam(':name',$name);
-  $create->bindParam(':surname',$surname);
-  $create->execute();
+      if ($value['username']==$username){
+        $exist = 1;
+        $message='Ce username est deja enregistré sur notre site!';
+        echo '<script>alert("'.$message.'");
+        window.location.href="index.php?page=register";
+        </script>';
+      }
+    }
+    if ($exist==0){
+      return 0;
+    }
+    else{
+      return 1;
+    }
+}
 
+  if (isset($_POST["Envoie"])){
+    $usermail =$_POST['email'];
+    $userpass=$_POST['password'];
+    $username=$_POST['username'];
+    $name=$_POST['name'];
+    $surname=$_POST['surname'];
+
+    $check = checkExist($db,$username,$usermail);
+
+    if ($check==0){
+    $userpass = password_hash($userpass,PASSWORD_DEFAULT);
+
+    $create=$bdd->prepare('INSERT INTO user VALUES (NULL, :usermail,:userpass,:username,:name,:surname)');
+    $create->bindParam(':usermail',$usermail);
+    $create->bindParam(':userpass',$userpass);
+    $create->bindParam(':username',$username);
+    $create->bindParam(':name',$name);
+    $create->bindParam(':surname',$surname);
+    $create->execute();
+
+
+    header('Location: index.php?page=account'); // redirige vers la page account
+    }
 }
 
 ?>
