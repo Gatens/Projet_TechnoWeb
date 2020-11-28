076c124a6d83f7bd922c823dd4c6d2c99449aced
champion <clem.champion59@gmail.com>
2020-11-28 17:43:57 +0100

    petits problèmes sur le commit d'avant donc je recommit avec la bonne version.

--- pages/register.php
+++ pages/register.php
@@ -1,8 +1,12 @@
-<form method="post" action="index.php?page=createUser">
- <label for="email">email</label> : <input type="email" name="email" placeholder="your email" id="email" required />
- <label for="password">password</label> : <input type="password" name="password" placeholder="password" id="password" required />
- <label for="username">username</label> : <input type="text" name="username" placeholder="your username" id="username" required />
- <label for="name">name</label> : <input type="text" name="name" placeholder="your name" id="name" required />
- <label for="surname">surname</label> : <input type="text" name="surname" placeholder="your surname" id="surname" required />
- <button type="submit" name="Envoie" href="index.php?page=account">Envoie</button>
+<div class="formulaire">
+
+<form class="register" method="post" action="index.php?page=createUser">
+ <label for="email">Adresse mail :</label> <input type="email" name="email" placeholder="Votre adresse mail" id="email" required />
+ <label for="password">Mot de passe :</label> <input type="password" name="password" placeholder="Votre mot de passe" id="password" required />
+ <label for="username">Pseudo :</label> <input type="text" name="username" placeholder="Votre pseudo" id="username" required />
+ <label for="name">Nom :</label> <input type="text" name="name" placeholder="Votre nom" id="name" required />
+ <label for="surname">Prénom :</label>  <input type="text" name="surname" placeholder="Votre prénom" id="surname" required />
+ <button class="inscription" type="submit" name="Envoie" href="index.php?page=account">S'inscrire</button>
 </form>
+
+</div>
